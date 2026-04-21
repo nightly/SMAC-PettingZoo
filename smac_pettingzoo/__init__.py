ded9ab87378a6c363b2fe7e4179b42ed431af121
nightly <47933317+nightly@users.noreply.github.com>
2026-04-22 00:52:24 +0100

    Fix modern protobuf packaging

---
+++ smac_pettingzoo/__init__.py
@@ -0,0 +1,6 @@
+from . import smacv1_pettingzoo_v1, smacv2_pettingzoo_v1
+
+__all__ = [
+    "smacv1_pettingzoo_v1",
+    "smacv2_pettingzoo_v1",
+]
---
+++ smac_pettingzoo/env/__init__.py
@@ -0,0 +1,7 @@
+from .smacv1 import SMACv1Env
+from .smacv2 import SMACv2Env
+
+__all__ = [
+    "SMACv1Env",
+    "SMACv2Env",
+]
